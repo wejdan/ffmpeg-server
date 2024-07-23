c5eb118391ea2ecf776ff0a36633ddec3fb9295c
wejdan <jode200951@hotmail.com>
2024-07-23 06:38:47 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,187 @@
+const express = require("express");
+const { Worker } = require("worker_threads");
+const path = require("path");
+const fs = require("fs");
+const axios = require("axios");
+const m3u8Parser = require("m3u8-parser");
+const bodyParser = require("body-parser");
+const { v4: uuidv4 } = require("uuid");
+const WebSocket = require("ws");
+
+const app = express();
+const PORT = 3000;
+let clients = {};
+// Middleware to parse JSON requests
+app.use(bodyParser.json());
+
+// Serve the downloaded files statically
+app.use("/downloads", express.static(path.join(__dirname, "downloads")));
+
+// Define a simple route
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+// WebSocket server
+const wss = new WebSocket.Server({ noServer: true });
+app.server = require("http").createServer(app);
+app.server.on("upgrade", (request, socket, head) => {
+  wss.handleUpgrade(request, socket, head, (ws) => {
+    wss.emit("connection", ws, request);
+  });
+});
+
+// Define a route to handle remuxing
+app.post("/remux", async (req, res, next) => {
+  try {
+    const { url, quality, name, videoId } = req.body;
+    const outputFileName = name || `${uuidv4()}.mp4`;
+    const outputPath = path.join(__dirname, "downloads", outputFileName);
+    const masterPlaylistContent = await axios
+      .get(url)
+      .then((response) => response.data);
+    const selectedStreams = await selectStreams(
+      masterPlaylistContent,
+      quality,
+      url
+    );
+
+    console.log("selectedStreams", selectedStreams);
+    if (!selectedStreams) {
+      return res
+        .status(400)
+        .send({ error: "Requested quality not found in the master playlist" });
+    }
+    // Create a WebSocket for this request
+    const ws = new WebSocket("ws://localhost:3000");
+
+    ws.on("open", async () => {
+      // Create a worker thread for remuxing
+      const worker = new Worker(path.join(__dirname, "worker.js"), {
+        workerData: {
+          videoUrl: selectedStreams.videoUrl,
+          audioUrl: selectedStreams.audioUrl,
+          outputPath,
+        },
+      });
+
+      worker.on("message", (message) => {
+        if (message.progress !== undefined) {
+          // console.log("message.progress", message.progress);
+          if (clients[videoId]) {
+            clients[videoId].send(
+              JSON.stringify({ progress: message.progress })
+            );
+          }
+        } else if (message.mp4Path) {
+          const relativePath = path.relative(__dirname, message.mp4Path);
+          const urlPath = `http://localhost:${PORT}/${relativePath
+            .split(path.sep)
+            .join("/")}`;
+          console.log("urlPath", urlPath);
+
+          ws.send(JSON.stringify({ done: true, mp4Path: urlPath }));
+          res.send({ mp4Path: urlPath });
+
+          setTimeout(() => {
+            fs.unlink(message.mp4Path, (err) => {
+              if (err) {
+                console.error(`Failed to delete file ${message.mp4Path}:`, err);
+              } else {
+                console.log(`File ${message.mp4Path} deleted`);
+              }
+            });
+          }, 600000); // Delete file after 1 minute
+        }
+      });
+
+      worker.on("error", (err) => {
+        if (clients[videoId]) {
+          clients[videoId].send(JSON.stringify({ error: err.message }));
+        }
+        res.status(500).send(err.message);
+      });
+
+      worker.on("exit", (code) => {
+        if (code !== 0) {
+          console.error(`Worker stopped with exit code ${code}`);
+          if (clients[videoId]) {
+            clients[videoId].send(
+              JSON.stringify({ error: `Worker stopped with exit code ${code}` })
+            );
+          }
+          res.status(500).send(`Worker stopped with exit code ${code}`);
+        }
+        if (clients[videoId]) {
+          clients[videoId].close();
+          delete clients[videoId];
+        }
+      });
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Function to select video and audio streams based on quality
+async function selectStreams(masterPlaylistContent, quality, baseUrl) {
+  const parser = new m3u8Parser.Parser();
+  parser.push(masterPlaylistContent);
+  parser.end();
+  console.log(masterPlaylistContent);
+  const { playlists, mediaGroups } = parser.manifest;
+
+  if (!playlists || playlists.length === 0) {
+    // Handle the case where the provided URL is not a master playlist
+    console.log("Provided URL is a single playlist");
+    const videoUrl = new URL(baseUrl).href;
+    return { videoUrl, audioUrl: null };
+  }
+  const [width, height] = quality.split("x").map(Number);
+  const selectedPlaylist = playlists.find(
+    (pl) =>
+      pl.attributes.RESOLUTION &&
+      pl.attributes.RESOLUTION.width === width &&
+      pl.attributes.RESOLUTION.height === height
+  );
+  if (!selectedPlaylist) {
+    throw new Error("Requested quality not found in the master playlist");
+  }
+
+  const videoUrl = new URL(selectedPlaylist.uri, baseUrl).href;
+  const audioGroupId = selectedPlaylist.attributes.AUDIO;
+  let audioUrl = null;
+
+  if (audioGroupId && mediaGroups.AUDIO && mediaGroups.AUDIO[audioGroupId]) {
+    const audioPlaylist = mediaGroups.AUDIO[audioGroupId];
+    const audioUriKey = Object.keys(audioPlaylist).find((key) =>
+      key.toLowerCase().includes("audio")
+    );
+
+    if (audioUriKey) {
+      audioUrl = new URL(audioPlaylist[audioUriKey].uri, baseUrl).href;
+    }
+  }
+
+  return { videoUrl, audioUrl };
+}
+
+// Global error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send({ error: err.message });
+});
+// Start the server
+app.server.listen(PORT, () => {
+  console.log(`Server is running and listening on port ${PORT}`);
+  wss.on("connection", (ws, request) => {
+    const urlParams = new URLSearchParams(request.url.slice(1));
+    const videoId = urlParams.get("videoId");
+    if (videoId) {
+      clients[videoId] = ws;
+      ws.on("close", () => {
+        delete clients[videoId];
+      });
+    }
+  });
+});
---
+++ worker.js
@@ -0,0 +1,49 @@
+const { workerData, parentPort } = require("worker_threads");
+const ffmpeg = require("fluent-ffmpeg");
+const path = require("path");
+
+// Set the path to the ffmpeg binary if necessary
+ffmpeg.setFfmpegPath("C:\\ffmpeg\\bin\\ffmpeg.exe");
+
+const { videoUrl, audioUrl, outputPath } = workerData;
+console.log("workerData", workerData);
+
+async function remuxHLS(videoUrl, audioUrl, outputMp4Path) {
+  return new Promise((resolve, reject) => {
+    const ffmpegCommand = ffmpeg();
+
+    ffmpegCommand.input(videoUrl);
+
+    if (audioUrl) {
+      ffmpegCommand.input(audioUrl);
+    }
+
+    ffmpegCommand
+      .outputOptions("-c copy")
+      .output(outputMp4Path)
+      .on("start", (commandLine) => {
+        console.log("Spawned Ffmpeg with command: " + commandLine);
+      })
+      .on("progress", (progress) => {
+        console.log(`Progress: ${progress.percent}% done`);
+        parentPort.postMessage({ progress: progress.percent });
+      })
+      .on("end", () => {
+        console.log("MP4 file created at:", outputMp4Path);
+        resolve(outputMp4Path);
+      })
+      .on("error", (err) => {
+        console.error("Error processing HLS:", err);
+        reject(new Error("Failed to process HLS"));
+      })
+      .run();
+  });
+}
+
+remuxHLS(videoUrl, audioUrl, outputPath)
+  .then((mp4Path) => {
+    parentPort.postMessage({ mp4Path });
+  })
+  .catch((error) => {
+    parentPort.postMessage({ error: error.message });
+  });
